3284398ba13746b9841948aa87bbe812228c275c
replace-all <thais.vnunes59@gmail.com>
2019-01-26 16:46:42 -0200

    criando arrays e listando as tarefas

--- task_list.rb
+++ task_list.rb
@@ -1,31 +1,56 @@
-tarefas = []
+@tarefas = []
+tarefa = ""
+
+def tarefas
+  return @tarefas # return aqui é desnecessario, mas deixei para ficar mais claro
+end
+
+def criar_tarefa()
+	puts 'Digite sua tarefa: '
+	tarefa = gets.chomp()
+	puts
+	puts "Tarefa cadastrada: #{tarefa} " 
+	sleep(2)	
+	tarefas.push(tarefa.capitalize)
+end
+
+def listar_tarefa()
+	puts				
+	#puts "Lista de tarefas: #{tarefas}" 
+	tarefas.each_with_index do |item, index|
+		puts "#{index + 1} = #{item}"
+	end
+	sleep(2)
+end
+
+def sair()
+	puts
+	puts 'Saindo...'
+	sleep(2)
+end
+
+def menu()
+	puts '[1] Inserir uma tarefa'
+	puts '[2] Ver todas as tarefas'
+	puts '[3] Sair'
+	puts
+	puts 'Opção escolhida: '
+end
+
+
 opcao = 0
 puts "Bem-vindo ao Task List! Escolha uma opção no menu: "
 	while opcao != 3 do
-		puts '[1] Inserir uma tarefa'
-		puts '[2] Ver todas as tarefas'
-		puts '[3] Sair'
-		puts
-		puts 'Opção escolhida: '	
+		menu()
 	
 	opcao = gets.chomp.to_i
-	
-
+		
   if opcao == 1
-    puts 'Digite sua tarefa: '
-		tarefa = gets.chomp()
-		tarefas.push(tarefa)
-    puts
-		puts 'Tarefa cadastrada: ' + tarefa
-		sleep(2)
+		criar_tarefa()
 	elsif opcao == 2
-		puts				
-		puts "Lista de tarefas: #{tarefas}" 
-		sleep(2)
+		listar_tarefa()
 	elsif opcao == 3
-		puts
-		puts 'Saindo...'
-		sleep(2)
+		sair()
   else
     puts
 		puts 'Opção inválida'		
